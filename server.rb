c637c6e69f39a29ab2a434963fef24bcf0d9243f
Jennifer He <jennifer@stripe.com>
2016-11-10 19:56:29 -0800

    Second commit

--- server.rb
+++ server.rb
@@ -1,82 +1,42 @@
 require 'sinatra'
 
+enable :sessions
 
+get "/" do
+  redirect '/index.html'
+end
 
 post '/cookie_submit' do
   puts params
   cookie_count = 0
-  cookie_result = NIL
-  cookie_message = NIL
 
-user_input=params["Question1"]
-  if user_input == "0"
-    cookie_count +=0
-  elsif user_input == "1"
-    cookie_count +=1
-  elsif user_input == "2"
-      cookie_count +=2
-  elsif user_input == "3"
-      cookie_count +=3
-  end
+["Question1", "Question2", "Question3", "Question4"].each do |user_input|
+  cookie_count += params[user_input].to_i
+end
 
-  user_input=params["Question2"]
-    if user_input == "0"
-      cookie_count +=0
-    elsif user_input == "1"
-      cookie_count +=1
-    elsif user_input == "2"
-        cookie_count +=2
-    elsif user_input == "3"
-        cookie_count +=3
-    end
-
-    user_input=params["Question3"]
-      if user_input == "0"
-        cookie_count +=0
-      elsif user_input == "1"
-        cookie_count +=1
-      elsif user_input == "2"
-          cookie_count +=2
-      elsif user_input == "3"
-          cookie_count +=3
-      end
-
-      user_input=params["Question4"]
-        if user_input == "0"
-          cookie_count +=0
-        elsif user_input == "1"
-          cookie_count +=1
-        elsif user_input == "2"
-            cookie_count +=2
-        elsif user_input == "3"
-            cookie_count +=3
-        end
-
-        user_input=params["Question5"]
-          if user_input == "0"
-            cookie_count +=0
-          elsif user_input == "1"
-            cookie_count +=1
-          elsif user_input == "2"
-              cookie_count +=2
-          elsif user_input == "3"
-              cookie_count +=3
-        end
 
         if cookie_count <= 3
-        	cookie_result = "glutenfree"
-        	cookie_message = "You are a Gluten Free cookie! You live life on the edge.. of your bed. You probably get more zzz's than most of your friends."
+        	session[:cookie_result] = "result1.png"
+        	session[:cookie_message] = "You are a Gluten Free cookie! You live life on the edge.. of your bed. You probably get more zzz's than most of your friends."
         elsif cookie_count >= 4 and cookie_count <= 7
-        	cookie_result = "chocochip"
-        	cookie_message = "You are a Chocolate Chip cookie! You embrace balance in life and know how to work hard but also play hard."
+        	session[:cookie_result] = "result2.png"
+        	session[:cookie_message] = "You are a Chocolate Chip cookie! You embrace balance in life and know how to work hard but also play hard."
         elsif cookie_count >= 8 and cookie_count <= 11
-        	cookie_result = "snickerdoodle"
-        	cookie_message = "You are a Snickerdoodle cookie! You generally follow the rules but are not afraid to push boundaries and challenge status quo."
+        	session[:cookie_result] = "result3.png"
+        	session[:cookie_message] = "You are a Snickerdoodle cookie! You generally follow the rules but are not afraid to push boundaries and challenge status quo."
         elsif cookie_count >= 12
-        	cookie_result = "funfetti"
-        	cookie_message = "You are a Funfetti cookie! Good news: You are the life of the party! Bad news: You are that quintessential crazy friend."
+        	session[:cookie_result] = "result4.png"
+        	session[:cookie_message] = "You are a Funfetti cookie! Good news: You are the life of the party! Bad news: You are that quintessential crazy friend."
         end
 
-        return cookie_message
+        redirect "/result"
 
+end
+
+get '/result' do
+  @cookie_result = session[:cookie_result]
+  @cookie_message = session[:cookie_message]
+  session.delete(:cookie_result)
+  session.delete(:cookie_message)
+  erb :result
 end
